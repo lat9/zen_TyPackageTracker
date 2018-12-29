83d6a128120ec100c67ac888511f0e2920e0bb06
James King <jeking@wheatonwebsiteservices.com>
2018-12-29 08:04:22 -0600

    Initial commit
    For Zen Cart 1.5.6

--- 2_Required_Core_Edits/includes/templates/custom_template/templates/tpl_account_history_info_default.php
+++ 2_Required_Core_Edits/includes/templates/custom_template/templates/tpl_account_history_info_default.php
@@ -9,34 +9,35 @@
  * @copyright Copyright 2003-2016 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Author: DrByte  Fri Jan 8 13:28:51 2016 -0500 Modified in v1.5.5 $
+ * @version $Id: Author: DrByte  May 3 2016  Modified in v1.5.5a $
  */
 ?>
+
 <div class="centerColumn" id="accountHistInfo">
-
-<div class="forward"><?php echo HEADING_ORDER_DATE . ' ' . zen_date_long($order->info['date_purchased']); ?></div>
-<br class="clearBoth" />
-
-<h2 id="orderHistoryDetailedOrder"><?php echo HEADING_TITLE . ORDER_HEADING_DIVIDER . sprintf(HEADING_ORDER_NUMBER, $_GET['order_id']); ?></h2>
-<table id="orderHistoryHeading">
+  <div class="forward"><?php echo HEADING_ORDER_DATE . ' ' . zen_date_long($order->info['date_purchased']); ?></div>
+  <br class="clearBoth" />
+  <?php if ($current_page != FILENAME_CHECKOUT_SUCCESS) { ?>
+  <h2 id="orderHistoryDetailedOrder"><?php echo HEADING_TITLE . ORDER_HEADING_DIVIDER . sprintf(HEADING_ORDER_NUMBER, $_GET['order_id']); ?></h2>
+  <?php } ?>
+  <table id="orderHistoryHeading">
     <tr class="tableHeading">
-        <th scope="col" id="myAccountQuantity"><?php echo HEADING_QUANTITY; ?></th>
-        <th scope="col" id="myAccountProducts"><?php echo HEADING_PRODUCTS; ?></th>
-<?php
+      <th scope="col" id="myAccountQuantity"><?php echo HEADING_QUANTITY; ?></th>
+      <th scope="col" id="myAccountProducts"><?php echo HEADING_PRODUCTS; ?></th>
+      <?php
   if (sizeof($order->info['tax_groups']) > 1) {
 ?>
-        <th scope="col" id="myAccountTax"><?php echo HEADING_TAX; ?></th>
-<?php
+      <th scope="col" id="myAccountTax"><?php echo HEADING_TAX; ?></th>
+      <?php
  }
 ?>
-        <th scope="col" id="myAccountTotal"><?php echo HEADING_TOTAL; ?></th>
+      <th scope="col" id="myAccountTotal"><?php echo HEADING_TOTAL; ?></th>
     </tr>
-<?php
+    <?php
   for ($i=0, $n=sizeof($order->products); $i<$n; $i++) {
   ?>
     <tr>
-        <td class="accountQuantityDisplay"><?php echo  $order->products[$i]['qty'] . QUANTITY_SUFFIX; ?></td>
-        <td class="accountProductDisplay"><?php echo  $order->products[$i]['name'];
+      <td class="accountQuantityDisplay"><?php echo  $order->products[$i]['qty'] . QUANTITY_SUFFIX; ?></td>
+      <td class="accountProductDisplay"><?php echo  $order->products[$i]['name'];
 
     if ( (isset($order->products[$i]['attributes'])) && (sizeof($order->products[$i]['attributes']) > 0) ) {
       echo '<ul class="orderAttribsList">';
@@ -45,71 +46,64 @@
       }
         echo '</ul>';
     }
-?>
-        </td>
-<?php
+?></td>
+      <?php
     if (sizeof($order->info['tax_groups']) > 1) {
 ?>
-        <td class="accountTaxDisplay"><?php echo zen_display_tax_value($order->products[$i]['tax']) . '%' ?></td>
-<?php
+      <td class="accountTaxDisplay"><?php echo zen_display_tax_value($order->products[$i]['tax']) . '%' ?></td>
+      <?php
     }
 ?>
-        <td class="accountTotalDisplay">
-        <?php
+      <td class="accountTotalDisplay"><?php
          $ppe = zen_round(zen_add_tax($order->products[$i]['final_price'], $order->products[$i]['tax']), $currencies->get_decimal_places($order->info['currency']));
          $ppt = $ppe * $order->products[$i]['qty'];
         //        echo $currencies->format(zen_add_tax($order->products[$i]['final_price'], $order->products[$i]['tax']) * $order->products[$i]['qty'], true, $order->info['currency'], $order->info['currency_value']) . ($order->products[$i]['onetime_charges'] != 0 ? '<br />' . $currencies->format(zen_add_tax($order->products[$i]['onetime_charges'], $order->products[$i]['tax']), true, $order->info['currency'], $order->info['currency_value']) : '')
         echo $currencies->format($ppt, true, $order->info['currency'], $order->info['currency_value']) . ($order->products[$i]['onetime_charges'] != 0 ? '<br />' . $currencies->format(zen_add_tax($order->products[$i]['onetime_charges'], $order->products[$i]['tax']), true, $order->info['currency'], $order->info['currency_value']) : '');
         ?></td>
     </tr>
-<?php
+    <?php
   }
 ?>
-</table>
-<hr />
-<div id="orderTotals">
-<?php
+  </table>
+  <hr />
+  <div id="orderTotals">
+    <?php
   for ($i=0, $n=sizeof($order->totals); $i<$n; $i++) {
 ?>
-     <div class="amount larger forward"><?php echo $order->totals[$i]['text'] ?></div>
-     <div class="lineTitle larger forward"><?php echo $order->totals[$i]['title'] ?></div>
-<br class="clearBoth" />
-<?php
+    <div class="amount larger forward"><?php echo $order->totals[$i]['text'] ?></div>
+    <div class="lineTitle larger forward"><?php echo $order->totals[$i]['title'] ?></div>
+    <br class="clearBoth" />
+    <?php
   }
 ?>
-
-</div>
-
-<?php
+  </div>
+  <?php
 /**
  * Used to display any downloads associated with the cutomers account
  */
   if (DOWNLOAD_ENABLED == 'true') require($template->get_template_dir('tpl_modules_downloads.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_modules_downloads.php');
 ?>
-
-
-<?php
+  <?php
 /**
  * Used to loop thru and display order status information
  */
 if (sizeof($statusArray)) {
 ?>
-
-<h2 id="orderHistoryStatus"><?php echo HEADING_ORDER_HISTORY; ?></h2>
-<table id="myAccountOrdersStatus">
+  <h2 id="orderHistoryStatus"><?php echo HEADING_ORDER_HISTORY; ?></h2>
+  <table id="myAccountOrdersStatus">
     <tr class="tableHeading">
-        <th scope="col" id="myAccountStatusDate"><?php echo TABLE_HEADING_STATUS_DATE; ?></th>
-        <th scope="col" id="myAccountStatus"><?php echo TABLE_HEADING_STATUS_ORDER_STATUS; ?></th>
-        <th scope="col" id="myAccountStatusComments"><?php echo TABLE_HEADING_STATUS_COMMENTS; ?></th>
-       </tr>
-<?php
+      <th scope="col" id="myAccountStatusDate"><?php echo TABLE_HEADING_STATUS_DATE; ?></th>
+      <th scope="col" id="myAccountStatus"><?php echo TABLE_HEADING_STATUS_ORDER_STATUS; ?></th>
+      <th scope="col" id="myAccountStatusComments"><?php echo TABLE_HEADING_STATUS_COMMENTS; ?></th>
+    </tr>
+    <?php
   foreach ($statusArray as $statuses) {
 ?>
-    <tr>
-<!-- Begin Ty Package Tracker -->
-        <td class="myAccountStatusComments"><?php echo zen_date_short($statuses['date_added']); ?></td>
-        <td class="myAccountStatusComments"><?php echo $statuses['orders_status_name']; ?></td>
-          <?php
+    <tr> 
+      <!-- Begin Ty Package Tracker -->
+      <td class="myAccountStatusComments"><?php echo zen_date_short($statuses['date_added']); ?></td>
+      <td class="myAccountStatusComments"><?php echo $statuses['orders_status_name']; ?></td>
+      <?php
 			$display_track_id = '';
 			$display_track_id .= (empty($statuses['track_id1']) ? '' : "<b>" . CARRIER_NAME_1 . "</b>" . ":&nbsp;&nbsp;<a href=" . CARRIER_LINK_1 . nl2br(zen_output_string_protected($statuses['track_id1'])) . ' target="_blank">' . nl2br(zen_output_string_protected($statuses['track_id1'])) . "</a>" );
 			$display_track_id .= (empty($statuses['track_id2']) ? '' : "<b>" . CARRIER_NAME_2 . "</b>" . ":&nbsp;&nbsp;<a href=" . CARRIER_LINK_2 . nl2br(zen_output_string_protected($statuses['track_id2'])) . ' target="_blank">' . nl2br(zen_output_string_protected($statuses['track_id2'])) . "</a>" );
@@ -117,45 +111,45 @@ if (sizeof($statusArray)) {
 			$display_track_id .= (empty($statuses['track_id4']) ? '' : "<b>" . CARRIER_NAME_4 . "</b>" . ":&nbsp;&nbsp;<a href=" . CARRIER_LINK_4 . nl2br(zen_output_string_protected($statuses['track_id4'])) . ' target="_blank">' . nl2br(zen_output_string_protected($statuses['track_id4'])) . "</a>" );
 			$display_track_id .= (empty($statuses['track_id5']) ? '' : "<b>" . CARRIER_NAME_5 . "</b>" . ":&nbsp;&nbsp;<a href=" . CARRIER_LINK_5 . nl2br(zen_output_string_protected($statuses['track_id5'])) . ' target="_blank">' . nl2br(zen_output_string_protected($statuses['track_id5'])) . "</a>" );
 			?>
-        <td class="myAccountStatusComments"><?php echo (empty($statuses['comments']) ? '&nbsp;' : nl2br(zen_output_string_protected($statuses['comments']))); ?><br>
-		<?php echo $display_track_id; ?><br></td>
-<!-- End Ty Package Tracker -->
-     </tr>
-<?php
+      <td class="myAccountStatusComments"><?php echo (empty($statuses['comments']) ? '&nbsp;' : nl2br(zen_output_string_protected($statuses['comments']))); ?><br>
+        <?php echo $display_track_id; ?><br></td>
+      <!-- End Ty Package Tracker --> 
+    </tr>
+    <?php
   }
 ?>
-</table>
-<?php } ?>
-
-<hr />
-<div id="myAccountShipInfo" class="floatingBox back">
-<?php
+  </table>
+  <?php } ?>
+  <hr />
+  <div id="myAccountShipInfo" class="floatingBox back">
+    <?php
   if ($order->delivery != false) {
 ?>
-<h3><?php echo HEADING_DELIVERY_ADDRESS; ?></h3>
-<address><?php echo zen_address_format($order->delivery['format_id'], $order->delivery, 1, ' ', '<br />'); ?></address>
-<?php
+    <h3><?php echo HEADING_DELIVERY_ADDRESS; ?></h3>
+    <address>
+    <?php echo zen_address_format($order->delivery['format_id'], $order->delivery, 1, ' ', '<br />'); ?>
+    </address>
+    <?php
   }
 ?>
-
-<?php
+    <?php
     if (zen_not_null($order->info['shipping_method'])) {
 ?>
-<h4><?php echo HEADING_SHIPPING_METHOD; ?></h4>
-<div><?php echo $order->info['shipping_method']; ?></div>
-<?php } else { // temporary just remove these 4 lines ?>
-<div>WARNING: Missing Shipping Information</div>
-<?php
+    <h4><?php echo HEADING_SHIPPING_METHOD; ?></h4>
+    <div><?php echo $order->info['shipping_method']; ?></div>
+    <?php } else { // temporary just remove these 4 lines ?>
+    <div>WARNING: Missing Shipping Information</div>
+    <?php
     }
 ?>
-</div>
-
-<div id="myAccountPaymentInfo" class="floatingBox forward">
-<h3><?php echo HEADING_BILLING_ADDRESS; ?></h3>
-<address><?php echo zen_address_format($order->billing['format_id'], $order->billing, 1, ' ', '<br />'); ?></address>
-
-<h4><?php echo HEADING_PAYMENT_METHOD; ?></h4>
-<div><?php echo $order->info['payment_method']; ?></div>
-</div>
-<br class="clearBoth" />
+  </div>
+  <div id="myAccountPaymentInfo" class="floatingBox forward">
+    <h3><?php echo HEADING_BILLING_ADDRESS; ?></h3>
+    <address>
+    <?php echo zen_address_format($order->billing['format_id'], $order->billing, 1, ' ', '<br />'); ?>
+    </address>
+    <h4><?php echo HEADING_PAYMENT_METHOD; ?></h4>
+    <div><?php echo $order->info['payment_method']; ?></div>
+  </div>
+  <br class="clearBoth" />
 </div>
