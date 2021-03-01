7690095850bf96fc29b31c1616e90abc51c156a0
lat9 <cindy@vinosdefrutastropicales.com>
2021-03-01 08:14:28 -0500

    #1: Remove template-override language file for account_history_info

---
+++ includes/languages/english/extra_definitions/ty_package_tracker_definitions.php
@@ -0,0 +1,11 @@
+<?php
+// -----
+// Part of the Ty Package Tracker plugin, v4.0.0 and later.
+//
+// Last updated 20210301-lat9 for v4.0.0
+//
+
+// -----
+// Definition, used for account_history_info page's status-table column heading.
+//
+define('TABLE_HEADING_TRACKING_ID', 'Tracking ID');
