e29932026952d5abf39f53b9e72aa28b7ded08ad
lat9 <cindy@vinosdefrutastropicales.com>
2021-03-02 13:56:48 -0500

    #3: Restructure uninstall SQL to individually remove configuration entries.

--- docs/ty_package_tracker/ty_package_tracker_uninstall.sql
+++ docs/ty_package_tracker/ty_package_tracker_uninstall.sql
@@ -1,12 +1,13 @@
-SELECT @tyid:=configuration_group_id FROM configuration_group
-WHERE configuration_group_title= 'Ty Package Tracker';
-DELETE FROM configuration WHERE configuration_group_id = @tyid;
-DELETE FROM configuration_group WHERE configuration_group_id = @tyid;
-DELETE FROM admin_pages WHERE page_key='configTyPackageTracker';
+DELETE FROM configuration WHERE configuration_key LIKE 'CARRIER_STATUS_%';
+DELETE FROM configuration WHERE configuration_key LIKE 'CARRIER_NAME_%';
+DELETE FROM configuration WHERE configuration_key LIKE 'CARRIER_LINK_%';
+DELETE FROM configuration WHERE configuration_key IN ('TY_TRACKER_VERSION', 'MAX_DISPLAY_PRODUCTS_IN_TRACK_ORDERS_BOX', 'TY_TRACKER');
+DELETE FROM configuration_group WHERE configuration_group_title = 'Ty Package Tracker' LIMIT 1;
+DELETE FROM admin_pages WHERE page_key = 'configTyPackageTracker' LIMIT 1;
 
 ALTER TABLE `orders_status_history` 
-DROP `track_id1`,
-DROP `track_id2`,
-DROP `track_id3`,
-DROP `track_id4`,
-DROP `track_id5`;
+    DROP `track_id1`,
+    DROP `track_id2`,
+    DROP `track_id3`,
+    DROP `track_id4`,
+    DROP `track_id5`;
