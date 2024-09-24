1583aaa88274d4b4e1b6d179935fff52dcb19f62
lat9 <lat9@vinosdefrutastropicales.com>
2024-09-24 10:42:25 -0400

    First, move files to their encapsulated directories

---
+++ zc_plugins/PackageTracker/v5.0.0/manifest.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'pluginVersion' => 'v5.0.0',
+    'pluginName' => 'Ty Package Tracker',
+    'pluginDescription' => 'This plugin adds the ability for your customers and admin to track packages from different carriers. See the plugin\'s <a href="https://github.com/lat9/zen_TyPackageTracker/wiki" target="_blank" rel="noopener noreferrer">Wiki</a> for additional information.',
+    'pluginAuthor' => 'Vinos de Frutas Tropicales (lat9)',
+    'pluginId' => 0, // ID from Zen Cart forum
+    'zcVersions' => [],
+    'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
+    'github_repo' => '', // url
+    'pluginGroups' => [],
+];
