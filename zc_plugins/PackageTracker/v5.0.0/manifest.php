da9576dd5dbc607c9e7ba34f0b0cf28b6d00fffb
lat9 <lat9@vinosdefrutastropicales.com>
2024-09-27 21:53:52 -0400

    Add pluginID to the manifest

--- zc_plugins/PackageTracker/v5.0.0/manifest.php
+++ zc_plugins/PackageTracker/v5.0.0/manifest.php
@@ -4,7 +4,7 @@ return [
     'pluginName' => 'Ty Package Tracker',
     'pluginDescription' => 'This plugin adds the ability for your customers and admin to track packages from different carriers. See the plugin\'s <a href="https://github.com/lat9/zen_TyPackageTracker/wiki" target="_blank" rel="noopener noreferrer">Wiki</a> for additional information.',
     'pluginAuthor' => 'Vinos de Frutas Tropicales (lat9)',
-    'pluginId' => 0, // ID from Zen Cart forum
+    'pluginId' => 75, // ID from Zen Cart forum
     'zcVersions' => [],
     'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
     'github_repo' => '', // url
